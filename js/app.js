e7ec72253c0b3ef42c7bfc2b7ef9bbdc356ab7d3
simonhfrost <simonhfrost@gmail.com>
2018-03-14 15:02:37 +1300

    Create lines with two dimensions

--- js/app.js
+++ js/app.js
@@ -3,28 +3,26 @@ const THREE = require('three')
 const initialize = require('./initializer.js').initialize
 const createAmbientLight = require('./object-creator.js').createAmbientLight
 const createDirectionalLight = require('./object-creator.js').createDirectionalLight
-const createCube = require('./object-creator.js').createCube
 
 const output = initialize()
 const scene = output.scene
 
-const loader = new THREE.JSONLoader()
+const drawLine = (a1, a2, b1, b2) => {
+  var curve = new THREE.LineCurve(
+    new THREE.Vector2(a1, a2),
+    new THREE.Vector2(b1, b2)
+  );
 
-var curve = new THREE.CubicBezierCurve(
-	new THREE.Vector2( -10, 0 ),
-	new THREE.Vector2( -5, 15 ),
-	new THREE.Vector2( 20, 15 ),
-	new THREE.Vector2( 10, 0 )
-);
+  var points = curve.getPoints( 10 )
+  var geometry = new THREE.BufferGeometry().setFromPoints( points )
+  var material = new THREE.LineBasicMaterial( { color : 0xff0000 } )
+  var curveObject = new THREE.Line( geometry, material )
 
-var points = curve.getPoints( 50 );
-var geometry = new THREE.BufferGeometry().setFromPoints( points );
+  scene.add(curveObject)
+}
 
-var material = new THREE.LineBasicMaterial( { color : 0xff0000 } );
 
-// Create the final object to add to the scene
-var curveObject = new THREE.Line( geometry, material );
+drawLine(-10, 0, 10, 0)
 
-scene.add(curveObject)
 scene.add(createAmbientLight())
 scene.add(createDirectionalLight())
